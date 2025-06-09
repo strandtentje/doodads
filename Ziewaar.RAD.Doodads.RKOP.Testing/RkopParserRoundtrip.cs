9b38c6fc3f7a66951fb63e83e543bab671727ab5
Rob Tierolff <teh@borreh.nl>
2025-06-09 10:15:50 +0200

    removed unused project, try to fix test

--- Ziewaar.RAD.Doodads.RKOP.Testing/RkopParserRoundtrip.cs
+++ Ziewaar.RAD.Doodads.RKOP.Testing/RkopParserRoundtrip.cs
@@ -11,13 +11,31 @@ namespace Ziewaar.RAD.Doodads.RKOP.Testing
             {
 
             }
-
-            public void SetDefinition<TResult>(CursorText atPosition, string typename, TResult? nextInLine, TResult? continuation, SortedList<string, object> constants, SortedList<string, TResult> wrappers) where TResult : IInstanceWrapper, new()
+            public void SetDefinition<TResult>(CursorText atPosition, string typename, SortedList<string, object> constants, SortedList<string, ServiceExpression<TResult>> wrappers) where TResult : class, IInstanceWrapper, new()
             {
                 
             }
-
-            void IInstanceWrapper.SetReference<TResult>(ServiceDescription<TResult> redirectsTo)
+            public void SetSoftLink<TResult>(ServiceExpression<TResult> redirectsTo) where TResult : class, IInstanceWrapper, new()
+            {
+                
+            }
+            public void SetHardLink<TResult>(ServiceExpression<TResult> redirectsTo) where TResult : class, IInstanceWrapper, new()
+            {
+                
+            }
+            public void SetUnconditionalSequence<TResult>(ServiceExpression<TResult>[] sequence) where TResult : class, IInstanceWrapper, new()
+            {
+                
+            }
+            public void SetAlternativeSequence<TResult>(ServiceExpression<TResult>[] sequence) where TResult : class, IInstanceWrapper, new()
+            {
+                
+            }
+            public void SetContinueSequence<TResult>(ServiceExpression<TResult>[] sequence) where TResult : class, IInstanceWrapper, new()
+            {
+                
+            }
+            public void SetDefinition<TResult>(CursorText atPosition, string typename, TResult? nextInLine, TResult? continuation, SortedList<string, object> constants, SortedList<string, TResult> wrappers) where TResult : IInstanceWrapper, new()
             {
                 
             }
