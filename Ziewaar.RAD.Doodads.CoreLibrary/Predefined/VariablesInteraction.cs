0e025b002213760377cb9222c5b595ed0c431c19
Rob Tierolff <rob@define-it.nl>
2025-06-26 22:39:02 +0200

    constructor extra simple interaction

--- Ziewaar.RAD.Doodads.CoreLibrary/Predefined/VariablesInteraction.cs
+++ Ziewaar.RAD.Doodads.CoreLibrary/Predefined/VariablesInteraction.cs
@@ -2,11 +2,23 @@
 
 #nullable enable
 
-public class CommonInteraction(IInteraction interaction, object? register = null, SortedList<string, object>? memory = null) : IInteraction
+public class CommonInteraction : IInteraction
 {
-    public IInteraction Stack => interaction;
-    public object Register { get; } = register ?? interaction.Register;
-    public IReadOnlyDictionary<string, object> Memory { get; } = memory ?? interaction.Memory;
+    public CommonInteraction(IInteraction interaction, object? register = null, SortedList<string, object>? memory = null)
+    {
+        this.Stack = interaction;
+        this.Register = register ?? interaction.Register;
+        this.Memory = memory ?? interaction.Memory;
+    }
+    public CommonInteraction(IInteraction interaction, IReadOnlyDictionary<string, object>? memory, object? register = null)
+    {
+        this.Stack = interaction;
+        this.Register = register ?? interaction.Register;
+        this.Memory = memory ?? interaction.Memory;
+    }
+    public IInteraction Stack { get; }
+    public object Register { get; }
+    public IReadOnlyDictionary<string, object> Memory { get; }
 }
 
 public class RootInteraction(object register, SortedList<string, object> memory) : ISelfStartingInteraction
---
+++ Ziewaar.RAD.Doodads.CoreLibrary/SwitchingDictionary.cs
@@ -0,0 +1,29 @@
+﻿namespace Ziewaar.RAD.Doodads.CoreLibrary;
+
+public class SwitchingDictionary(
+    string[] members,
+    Func<string, object> valueSource) : IReadOnlyDictionary<string, object>
+{
+    public object this[string key] => key switch
+    {
+        null => throw new ArgumentNullException(),
+        _ => valueSource(key)
+    };
+
+    public IEnumerable<string> Keys => members;
+    public IEnumerable<object> Values => members.Select(x => valueSource(x));
+    public int Count { get; } = members.Length;
+    public bool ContainsKey(string key) => members.Contains(key);
+    public IEnumerator<KeyValuePair<string, object>> GetEnumerator() =>
+        members.ToDictionary(x => x, valueSource).GetEnumerator();
+    public bool TryGetValue(string key, out object value)
+    {
+        value = null;
+        if (key == null) return false;
+        if (!members.Contains(key)) return false;
+        value = valueSource(key);
+        return true;
+    }
+    IEnumerator IEnumerable.GetEnumerator() =>
+        members.ToDictionary(x => x, valueSource).GetEnumerator();
+}
