c086bd04702ce193191ab77c24d28e3b339c6c82
Rob Tierolff <teh@borreh.nl>
2025-07-16 22:50:33 +0200

    lets split strings if its strings.

--- Ziewaar.RAD.Doodads.ModuleLoader/Exceptions/ExceptionPayload.cs
+++ Ziewaar.RAD.Doodads.ModuleLoader/Exceptions/ExceptionPayload.cs
@@ -9,7 +9,7 @@ public class ExceptionPayload
         PrimaryConstantStamp = consts?.PrimaryLog ?? -1;
         Constants = consts?.NamedItems.Join(consts?.ChangeLog, x => x.Key, x => x.Key, DiagnosticConstant.Create) ?? [];
         CurrentTimeStamp = GlobalStopwatch.Instance.ElapsedTicks;
-        Register = interaction?.Register?.ToString() ?? "Empty Register";
+        Register = interaction?.Register?.ToString().Split('\n') ?? ["Empty Register"];
         Type = type?.Name ?? "Unknown Type";
         Directory = text?.WorkingDirectory?.FullName ?? "No Directory";
         File = text?.BareFile ?? "No File";
@@ -19,7 +19,8 @@ public class ExceptionPayload
             Select(x => (x.Key, x.Value.ToString())).
             ToDictionary(x => x.Key, x => x.Item2);
     }
-    public readonly string Register, Type, Directory, File, PrimaryConstant;
+    public readonly string[] Register;
+    public readonly string  Type, Directory, File, PrimaryConstant;
     public readonly long PrimaryConstantStamp, CurrentTimeStamp;
     public readonly int Line, Column;
     public readonly IReadOnlyDictionary<string, string> Memory;
