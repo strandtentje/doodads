f10e149fa5370dc7f86d463fa1e11f1618ff6210
SanderdeFine <166691890+SanderdeFine@users.noreply.github.com>
2025-08-05 13:28:16 +0200

    dump bug, json bug

--- Ziewaar.RAD.Doodads.FormsValidation/Common/JsonDecodingQueryDictionary.cs
+++ Ziewaar.RAD.Doodads.FormsValidation/Common/JsonDecodingQueryDictionary.cs
@@ -10,12 +10,19 @@ public class JsonDecodingQueryDictionary(
     private Dictionary<string, object>? BackingStore;
     public static IDecodingDictionary CreateFor(string encodedData, string[] unsanitizedWhitelist) =>
         new JsonDecodingQueryDictionary(
-            unsanitizedWhitelist.Select(JsonConvert.ToString).ToArray(),
+            unsanitizedWhitelist,
             encodedData);
     protected override object? FindValue(string sanitizedKey)
     {
         this.BackingStore ??= JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonEncodedQuery);
-        return this.BackingStore?.GetValueOrDefault(sanitizedKey);
+        if (this.BackingStore!.TryGetValue(sanitizedKey, out object? value))
+        {
+            return value;
+        }
+        else
+        {
+            return null;
+        }
     }
     protected override string Sanitize(string unsanitized) => unsanitized;
     protected override string Desanitize(string sanitized) => sanitized;
