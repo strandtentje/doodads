dd647290e70a2b666b37414d911ef88affb200e7
Rob Tierolff <rob@define-it.nl>
2025-10-01 18:04:17 +0200

    concat stream finish sensing duh

--- Ziewaar.RAD.Doodads.EnumerableStreaming/ConcatenatedStream.cs
+++ Ziewaar.RAD.Doodads.EnumerableStreaming/ConcatenatedStream.cs
@@ -1,18 +1,23 @@
+using Ziewaar.RAD.Doodads.CoreLibrary;
+
 namespace Ziewaar.RAD.Doodads.EnumerableStreaming;
 #pragma warning disable 67
-public sealed class ConcatenatedStream(params Stream[] streams) : Stream
+public sealed class ConcatenatedStream(params Stream[] streams) : Stream, IFinishSensingStream
 {
     private int CurrentStreamNumber;
     public override bool CanRead => true;
     public override bool CanSeek => false;
     public override bool CanWrite => false;
     public override long Length => throw new NotSupportedException();
+
     public override long Position
     {
         get => throw new NotSupportedException();
         set => throw new NotSupportedException();
     }
+
     public override void Flush() => throw new NotSupportedException();
+
     public override int Read(byte[] buffer, int offset, int count)
     {
         while (CurrentStreamNumber < streams.Length)
@@ -21,14 +26,19 @@ public sealed class ConcatenatedStream(params Stream[] streams) : Stream
             if (r > 0) return r;
             CurrentStreamNumber++;
         }
+
         return 0;
     }
+
     public override long Seek(long offset, SeekOrigin origin) =>
         throw new NotSupportedException();
+
     public override void SetLength(long value) =>
         throw new NotSupportedException();
+
     public override void Write(byte[] buffer, int offset, int count) =>
         throw new NotSupportedException();
+
     protected override void Dispose(bool disposing)
     {
         if (disposing)
@@ -36,6 +46,11 @@ public sealed class ConcatenatedStream(params Stream[] streams) : Stream
             foreach (var s in streams)
                 s.Dispose();
         }
+
         base.Dispose(disposing);
     }
+
+    public bool IsFinished =>
+        CurrentStreamNumber >= streams.Length - 1
+        && streams[CurrentStreamNumber] is IFinishSensingStream { IsFinished: true };
 }
