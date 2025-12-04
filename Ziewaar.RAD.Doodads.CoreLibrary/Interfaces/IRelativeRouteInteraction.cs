1c78dd7694503406f14c2988e73415a18431b3e9
Rob Tierolff <rob@define-it.nl>
2025-12-04 15:46:46 +0100

    Include current path in url also

--- Ziewaar.RAD.Doodads.CoreLibrary/Interfaces/IRelativeRouteInteraction.cs
+++ Ziewaar.RAD.Doodads.CoreLibrary/Interfaces/IRelativeRouteInteraction.cs
@@ -2,5 +2,6 @@ namespace Ziewaar.RAD.Doodads.CoreLibrary.Interfaces;
 
 public interface IRelativeRouteInteraction : IInteraction
 {
+    string CurrentLocation { get; }
     IEnumerable<string> Remaining { get; }
 }
