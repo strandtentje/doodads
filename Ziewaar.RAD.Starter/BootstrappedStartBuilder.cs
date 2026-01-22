760480dae48c0231d6ec6f35fc4e0254bc5f5ec3
Rob Tierolff <rob@define-it.nl>
2026-01-22 11:26:26 +0100

    old style load assembly by file just breaks so many things so we're yeeting that. i don't think that was actually ever _used_

--- Ziewaar.RAD.Starter/BootstrappedStartBuilder.cs
+++ Ziewaar.RAD.Starter/BootstrappedStartBuilder.cs
@@ -14,20 +14,18 @@ public class BootstrappedStartBuilder
     private string _workingDirectory;
     public string WorkingDirectory => _workingDirectory;
     private readonly List<(string name, string definition)> _fileDefinitions = new();
-    private readonly List<(string path, Assembly assembly)> _assemblyFileNames = new();
+    private readonly HashSet<Assembly> _assembliesToLoad = new();
     private SortedList<string, object> _rootContext = new();
     private string? StarterFile;
     public IReadOnlyList<(string name, string definition)> FileDefinitions => _fileDefinitions;
-    public IReadOnlyList<(string path, Assembly assembly)> AssembliesToPopulate => _assemblyFileNames;
+    public IEnumerable<Assembly> AssembliesToLoad => _assembliesToLoad;
     public IReadOnlyDictionary<string, object> RootContext => _rootContext;
-    public string? RuntimeAssemblyFile { get; private set; }
     private BootstrappedStartBuilder(string? workingDirectory) => this._workingDirectory = workingDirectory ?? Environment.CurrentDirectory;
     public static BootstrappedStartBuilder Create(string workingDirectory) => new BootstrappedStartBuilder("".QualifyFullPath(workingDirectory));
     public BootstrappedStartBuilder ReadArgs(string[] args)
     {
         var x = ArgParser.Parse(args);
         var workingDirectory = x.Options.GetValuesStartingWith<string>("w").SingleOrDefault();
-        var assemblyFiles = x.Options.GetValuesStartingWith<string>("a");
         var bootFiles = x.Options.GetValuesStartingWith<string>("b");
         if (bootFiles.Length == 0 && x.Filenames.Count > 0)
             bootFiles = [.. x.Filenames];
@@ -35,12 +33,7 @@ public class BootstrappedStartBuilder
             x.Options.GetValuesStartingWith<string>("s").SingleOrDefault()
             ?? bootFiles.LastOrDefault();
 
-        _workingDirectory = workingDirectory ?? _workingDirectory;
-        if (assemblyFiles.Any())
-        {
-            foreach (var item in assemblyFiles)
-                AddAssembly(item);
-        }
+        _workingDirectory = workingDirectory ?? _workingDirectory;        
         if (bootFiles.Any())
         {
             _fileDefinitions.Clear();
@@ -51,7 +44,7 @@ public class BootstrappedStartBuilder
         {
             this.SetStarter(starterFile);
         }
-        var rootContext = x.Options.RemoveValuesStartingWithAny("w", "a", "b", "s");
+        var rootContext = x.Options.RemoveValuesStartingWithAny("w", "b", "s");
         if (rootContext.Any())
             this.SetRootContext(rootContext);
         return this;
@@ -102,55 +95,25 @@ public class BootstrappedStartBuilder
         return this;
     }
     public string TrimmedWorkingDirectory => _workingDirectory.TrimEnd(Path.DirectorySeparatorChar);
-    public BootstrappedStartBuilder SetRuntimeAssemblyFile(string file)
-    {
-        file = ExecutableResolver.FindAssociatedExecutable(file);
-        RuntimeAssemblyFile = file.TruncatePathStart(_workingDirectory);        
-        return this;
-    }
-    public BootstrappedStartBuilder SetRuntimeAssembly(Assembly assembly) => SetRuntimeAssemblyFile(assembly.Location);
-    public BootstrappedStartBuilder SetRuntimeBy<TType>() => SetRuntimeAssembly(typeof(TType).Assembly);
-    public BootstrappedStartBuilder AddAssembly(string path, Assembly? assembly = null)
-    {
-        if (_assemblyFileNames.Any(x => x.path == path))
-            return this;        
-        Assembly selectedAssembly = assembly ?? Assembly.LoadFile(path.QualifyFullPath(_workingDirectory));
-        string selectedPath = path.TruncatePathStart(_workingDirectory);
-        if (selectedAssembly.Location.QualifyFullPath(_workingDirectory) != selectedPath.QualifyFullPath(_workingDirectory))
-            throw new ArgumentException("path does not match assembly location");
-        _assemblyFileNames.Add((selectedPath, selectedAssembly));
-        return this;
-    }
     public BootstrappedStartBuilder AddAssembly(Assembly assembly)
     {
-        return AddAssembly(assembly.Location, assembly);
+        _assembliesToLoad.Add(assembly);    
+        return this;
     }
     public BootstrappedStartBuilder AddAssemblyBy<TType>() => AddAssembly(typeof(TType).Assembly);
     public string[] BuildArgs()
     {
-        var workingDirectoryFlags = _workingDirectory.ToFilenameFlags("w");
-        var assemblyFileFlags = AssembliesToPopulate.ToMultipleFilenameFlags("a", x => x.path);
+        var workingDirectoryFlags = _workingDirectory.ToFilenameFlags("w");        
         var bootDefFlags = FileDefinitions.ToMultipleFilenameFlags("b", x => x.name);
         var starterFileFlags = (StarterFile ?? FileDefinitions.Last().name).ToFilenameFlags("s");
         return
         [
             .. workingDirectoryFlags,
-            .. assemblyFileFlags,
+            "",
             .. bootDefFlags,
             .. starterFileFlags,
         ];
     }
-    public ProcessStartInfo BuildProcessStart(bool wrap) => new ProcessStartInfo(
-        RuntimeAssemblyFile ?? throw new InvalidOperationException("Runtime assembly must be provided"),
-        string.Join(" ", BuildArgs()))
-    {
-        WorkingDirectory = this.WorkingDirectory,
-        CreateNoWindow = wrap,
-        UseShellExecute = !wrap,
-        RedirectStandardError = wrap,
-        RedirectStandardInput = wrap,
-        RedirectStandardOutput = wrap,
-    };
     private string[] RequireFullyQualifiedDefinitionFilePaths() =>
         FileDefinitions.Select(x => x.name.QualifyFullPath(WorkingDirectory)).ToArray();
     private string RequireFullyQualifiedStarterFilePath() =>
@@ -159,7 +122,7 @@ public class BootstrappedStartBuilder
         QualifyFullPath(WorkingDirectory);
     public BootstrappedStart Build() => new BootstrappedStart(
         workingDirectory: _workingDirectory,
-        populateAssemblies: AssembliesToPopulate.Select(x => x.assembly).ToArray(),
+        populateAssemblies: _assembliesToLoad.ToArray(),
         loadFiles: RequireFullyQualifiedDefinitionFilePaths(),
         startFile: RequireFullyQualifiedStarterFilePath(),
         rootInteractionMemory: _rootContext
