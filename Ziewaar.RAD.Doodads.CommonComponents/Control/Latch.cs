97f0ad21a228bcb9b5077fa8b23789d322354b9c
Rob Tierolff <rob@define-it.nl>
2026-02-10 17:01:18 +0100

    more dumb latch

--- Ziewaar.RAD.Doodads.CommonComponents/Control/Latch.cs
+++ Ziewaar.RAD.Doodads.CommonComponents/Control/Latch.cs
@@ -36,3 +36,31 @@ public class Latch : IService
 
     public void HandleFatal(IInteraction source, Exception ex) => OnException?.Invoke(this, source);
 }
+
+[Category("Scheduling & Flow")]
+[Title("At Most One At A Time")]
+[Description("""
+    Passes through control transparently, but makes sure of whatever is running 
+    on the OnThen branch, it's only one; the rest of the interactions will be dropped.
+    """)]
+public class BlockingLatch : IService
+{
+    private readonly object lockObject = new();
+    private bool isCaptured = false;
+    [EventOccasion("Runs one at a time using the provided interaction")]
+    public event CallForInteraction? OnThen;
+    [EventOccasion("If someone else is in here already, this happens.")]
+    public event CallForInteraction? OnElse;
+    [NeverHappens]
+    public event CallForInteraction? OnException;
+
+    public void Enter(StampedMap constants, IInteraction interaction)
+    {
+        lock (lockObject)
+        {
+            OnThen?.Invoke(this, interaction);     
+        }
+    }
+
+    public void HandleFatal(IInteraction source, Exception ex) => OnException?.Invoke(this, source);
+}
