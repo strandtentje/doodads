37e0216f532d9ca3047bfb8f7063bd773ca38862
Rob <rob@define-it.nl>
2026-09-07 15:18:20 +0200

    useful redirect jig

---
+++ Ziewaar.RAD.Doodads.StandaloneWebserver/Services/HttpRedirectFormat.cs
@@ -0,0 +1,28 @@
+﻿#pragma warning disable 67
+using Define.Doodads.Expo.Timeline;
+using Ziewaar.RAD.Doodads.CommonComponents.TextTemplating;
+
+namespace Ziewaar.RAD.Doodads.StandaloneWebserver.Services;
+
+[Category("Http & Routing")]
+[Title("Http redirect to formatted")]
+[Description("""
+             Does a 307 Temporary Redirect to the path template in primary constant
+             """)]
+[ShortNames("fredir")]
+public class HttpRedirectFormat : BasicService
+{
+    private StampedMap? Constants;
+    private readonly Format FormatService = new();
+    private readonly HttpRedirect RedirectService = new();
+    public HttpRedirectFormat()
+    {
+        RedirectService.OnThen += (s, e) => FormatService.Enter(Constants!, e);
+        RedirectService.OnException += (s, e) => throw new BasicException(e.Register.ToString());
+    }
+    public override void TryEnter(StampedMap constants, IInteraction interaction)
+    {
+        this.Constants = constants;
+        RedirectService.Enter(this.Constants, interaction);
+    }
+}
